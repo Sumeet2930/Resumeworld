1fd417d9a9ca9c8f831d5f354848b34d72c8eabd
Sumeet <sumitkumarsahoo2930@gmail.com>
2026-03-28 00:41:33 +0530

    Add Vercel Analytics integration and global styles to the frontend.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/globals.css';
 import '../styles/styles.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Analytics />
+      <Component {...pageProps} />
+    </>
+  );
 }
